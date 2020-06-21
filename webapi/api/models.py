3e3aec030cf35f9de717b46d3bb6d46389b7bfd9
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2020-06-21 22:18:43 +0900

    migrate

--- webapi/api/models.py
+++ webapi/api/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
