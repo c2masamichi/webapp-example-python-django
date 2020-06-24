7a67a9656bebec83a89c9184ba81b3af1201ea7e
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2020-06-24 22:39:30 +0900

    add test

--- webapi/api/tests.py
+++ webapi/api/tests.py
@@ -1,3 +1,8 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class ProductsViewTests(TestCase):
+    def test_get_products(self):
+        response = self.client.get(reverse('api_v1:list'))
+        self.assertEqual(response.status_code, 200)
--- webapi/api/urls.py
+++ webapi/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'api_v1'
 urlpatterns = [
     path('products', views.get_products, name='list'),
 ]
