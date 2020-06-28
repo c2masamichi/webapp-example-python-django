49d08635a5dfe830c2d6296814bc62db59057504
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2020-06-28 21:15:16 +0900

    add setup

--- webapi/api/tests.py
+++ webapi/api/tests.py
@@ -3,8 +3,14 @@ import json
 from django.urls import reverse
 from django.test import TestCase
 
+from api.models import Product
+
 
 class ProductsViewTests(TestCase):
+    def setUp(self):
+        Product.objects.create(name='book', price=600)
+        Product.objects.create(name='fish', price=200)
+
     def test_get_products(self):
         response = self.client.get(reverse('api_v1:list'))
         self.assertEqual(response.status_code, 200)
