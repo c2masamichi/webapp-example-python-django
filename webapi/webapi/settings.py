7a41ea1558afead3a8f0d7ccbf4f2d09c27ae0d1
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2020-07-29 18:57:27 +0900

    sqlite3 to mysql

--- webapi/webapi/settings.py
+++ webapi/webapi/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dev_db',
+        'USER': 'dev_user',
+        'PASSWORD': 'dev_pass',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
