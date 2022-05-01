c3e1d23c09004017abd2445432afe08dd6bed1a9
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2022-05-01 17:12:01 +0900

    fix test data

--- cms/app/tests/test_views.py
+++ cms/app/tests/test_views.py
@@ -5,7 +5,7 @@ import pytest
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Test Title 1' in response.content
+    assert b'Test Title 01' in response.content
     assert b'2019-01-01' in response.content
 
 
@@ -13,12 +13,12 @@ def test_index(client):
 def test_detail(client):
     response = client.get('/entry/1')
     assert response.status_code == 200
-    assert b'Test Title 1' in response.content
+    assert b'Test Title 01' in response.content
     assert b'2019-01-01' in response.content
     assert b'This body is test.' in response.content
 
 
 @pytest.mark.django_db
 def test_detail_exists_required(client):
-    response = client.get('/entry/10')
+    response = client.get('/entry/100')
     assert response.status_code == 404
